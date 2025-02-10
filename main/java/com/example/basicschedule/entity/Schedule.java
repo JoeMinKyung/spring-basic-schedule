70077b9530109a900db5c42882645dc2cbbbae02
minkyungCHO <mk981015@naver.com>
2025-02-10 18:22:10 +0900

    feat: Entity에 id, 작성자, 작성일, 수정일, 마감일, 수행 여부, 할 일 추가 및 생성자 추가

---
+++ main/java/com/example/basicschedule/entity/Schedule.java
@@ -0,0 +1,44 @@
+package com.example.basicschedule.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class) // 자동 날짜 업데이트 활성화
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String writer;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deadline;
+
+    private boolean isCompleted;
+
+    private String task;
+
+    public Schedule(String writer, LocalDateTime deadline, String task) {
+        this.writer = writer;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.deadline = deadline;
+        this.isCompleted = false;
+        this.task = task;
+    }
+}
